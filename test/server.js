ccdbab24565ef56b46f965c821e40aa3867790b2
Bismark Henao <bismarkhenao7@gmail.com>
2018-07-30 14:23:56 +0200

    Remove test

--- test/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const fallback = require('express-history-api-fallback');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fallback('index.html', { root: path.join(__dirname, 'public') }));
-app.listen(process.env.PORT || 3000);
